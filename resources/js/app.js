622fd7134e6841d0c094a597cbf8853d5c3333ba
Mohamedfadelzergou <mohamedfadelzergou@gmail.com>
2021-12-20 23:46:32 +0100

    Installing Vuejs and Boostrap Vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 require('./bootstrap');
+window.Vue=require('vue');
+window.Fire=new Vue();
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
