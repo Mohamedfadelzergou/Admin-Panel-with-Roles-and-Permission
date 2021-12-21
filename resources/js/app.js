d9b8dec055adba84f62e3262ddef8fe9ec5044d7
Mohamedfadelzergou <mohamedfadelzergou@gmail.com>
2021-12-21 12:39:30 +0100

    fix bugs vuejs and Vue Components

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,14 @@
 require('./bootstrap');
-window.Vue=require('vue');
+window.Vue = require('vue');
 window.Fire=new Vue();
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import Vue from 'vue';
 
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue)
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
+Vue.component('role',require('./components/role.vue').default);
+const app = new Vue({
+    el: '#app'
+});
