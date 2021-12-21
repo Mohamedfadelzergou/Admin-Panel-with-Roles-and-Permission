7442eab58e1efdbab51569d3e23c945530a43ece
Mohamedfadelzergou <mohamedfadelzergou@gmail.com>
2021-12-21 12:48:25 +0100

    Installing SweetAlert

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,6 +9,22 @@ Vue.use(BootstrapVue)
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
 Vue.component('role',require('./components/role.vue').default);
+//sweetalert
+import Swal from 'sweetalert2'
+window.swal = Swal;
+const Toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener('mouseenter', Swal.stopTimer)
+      toast.addEventListener('mouseleave', Swal.resumeTimer)
+    }
+});
+
+window.toast = Toast;
 const app = new Vue({
     el: '#app'
 });
